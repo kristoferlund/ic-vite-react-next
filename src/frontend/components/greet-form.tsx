2a4b602025ad6dd04de76ba7fb8bcb28f72799c7
b3hr4d <behradmusiclove@yahoo.com>
2024-11-30 14:25:43 +0300

    Add Prettier configuration, remove unused hook, and update greet form to use new API

--- src/frontend/components/greet-form.tsx
+++ src/frontend/components/greet-form.tsx
@@ -1,36 +1,70 @@
-import { useState } from "react";
-import useGreet from "../hooks/use-greet"
-import Bubble from "./bubble";
+import { useQueryCall } from "@ic-reactor/react"
+import { useState } from "react"
+import { _SERVICE } from "../../backend/declarations/backend.did"
+import Bubble from "./bubble"
 
 type TimedBubble = {
-  id: number,
+  id: number
   bubble: React.JSX.Element
 }
 
 export default function GreetForm() {
-  const [name, setName] = useState("");
-  const [bubbles, setBubbles] = useState<TimedBubble[]>([]);
+  const [name, setName] = useState("")
+  const [bubbles, setBubbles] = useState<TimedBubble[]>([])
 
   const addBubble = (greeting: string) => {
-    const id = Date.now();
-    setBubbles((prev) => [...prev, {
-      id,
-      bubble: <Bubble key={id} text={greeting} onComplete={() => { removeBubble(id); }} />
-    }]);
-  };
+    const id = Date.now()
+    setBubbles((prev) => [
+      ...prev,
+      {
+        id,
+        bubble: (
+          <Bubble
+            key={id}
+            text={greeting}
+            onComplete={() => {
+              removeBubble(id)
+            }}
+          />
+        ),
+      },
+    ])
+  }
 
   const removeBubble = (id: number) => {
-    setBubbles((prev) => prev.filter((tb) => tb.id !== id));
-  };
+    setBubbles((prev) => prev.filter((tb) => tb.id !== id))
+  }
 
-  const { mutate: greet, isPending } = useGreet(addBubble);
+  const { refetch, loading } = useQueryCall<_SERVICE>({
+    functionName: "greet",
+    refetchOnMount: false,
+    args: [name],
+    onSuccess: addBubble,
+  })
 
-  return <div className="flex flex-col bg-[#522785] p-10 rounded-xl items-center text-xl text-white gap-5">
-    <div>Hello stranger, what&apos;s your name?</div>
-    <div className="w-full flex flex-col gap-2">
-      <input type="text" value={name} placeholder="Name" onChange={(e) => { setName(e.target.value); }} className="w-full block rounded-md py-2.5 px-3.5 text-center border focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 shadow-sm focus-visible:outline-white" data-1p-ignore />
-      <button onClick={() => { greet(name); }} disabled={isPending} className="w-full block rounded-md py-2.5 px-3.5 text-center text-[#522785] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/30 shadow-sm hover:bg-white/50 focus-visible:outline-white">Greet</button>
+  return (
+    <div className="flex flex-col bg-[#522785] p-10 rounded-xl items-center text-xl text-white gap-5">
+      <div>Hello stranger, what&apos;s your name?</div>
+      <div className="w-full flex flex-col gap-2">
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+          className="w-full block rounded-md py-2.5 px-3.5 text-center border focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 shadow-sm focus-visible:outline-white"
+          data-1p-ignore
+        />
+        <button
+          onClick={refetch}
+          disabled={loading}
+          className="w-full block rounded-md py-2.5 px-3.5 text-center text-[#522785] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/30 shadow-sm hover:bg-white/50 focus-visible:outline-white"
+        >
+          Greet
+        </button>
+      </div>
+      {bubbles.map((tb) => tb.bubble)}
     </div>
-    {bubbles.map((tb) => tb.bubble)}
-  </div >
+  )
 }
