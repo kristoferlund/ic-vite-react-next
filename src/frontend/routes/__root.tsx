4ecd71650a7eacc068b23d9e266ed01e8f6a81d1
Kristofer <kristofer@fmckl.se>
2025-04-02 11:17:43 +0200

    Styling updates

--- src/frontend/routes/__root.tsx
+++ src/frontend/routes/__root.tsx
@@ -7,7 +7,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <main className="dark">
-      <div className="flex flex-col gap-14 items-center">
+      <div className="flex flex-col gap-14 items-center w-[400px]">
         <div className="flex gap-10">
           <a
             href="https://internetcomputer.org"
@@ -23,7 +23,11 @@ export const Route = createRootRoute({
             <img src={reactLogo} alt="React logo" className="h-20" />
           </a>
         </div>
-        <h1>ICP + Vite + React</h1>
+        <h1>ICP Starter</h1>
+        <h2 className="text-center">
+          React + TypeScript + Vite + Tailwind + Shadcn/UI + Tanstack
+          Query/Router
+        </h2>
         <div className="flex gap-5 text-white underline">
           <Link to="/">/index</Link>
           <Link to="/about">/about</Link>
